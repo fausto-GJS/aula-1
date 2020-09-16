1c1c4804bf2a2a603ee2495d0cc8e72b60c1743d
Fausto Garcia Jacinto <faustogarciajacinto@gmail.com>
2020-09-16 23:40:33 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void) {
+  printf("Olá mundo\n");
+  return 0;
+}
+/*
+Pseudocodigo | C
+inteiro      = int(%d)
+real         = floar(%f) ou double(%lf)
+caracter     = char (%c)
+cadeia       = char[10] (%s)
+-------------------------------------
+escreva("ok") = printf("OK");
+leia(dia)     = scanF("%d,&dia");
+--------------------------------------
+Idade:25 anos.
+escreva("Idade: ",Idade," anos\n");
+printf("Idade: %d anos\n", idade);
+--------------------------------------
+leia(idade, peso)
+scanf("%d%f", &idade, &peso);
+printf("Idade = %d e o Peso = %f \n", idade,peso);
+*/
